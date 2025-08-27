ca348f3e26d27e6f40aeff172b5021ec4c9774b3
WenyuanXu <wenyuan.xu@cs.au.dk>
2025-08-27 13:39:01 +0200

    30GB limit

--- generate_graph.py
+++ generate_graph.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
+import resource
+
 from simurun.launcher import main
+
+# Set maximum heap size (e.g., 500 MB)
+max_heap_size = 30 * 1024 * 1024 * 1024  # 500 MB in bytes
+
+# Set memory limit
+resource.setrlimit(resource.RLIMIT_AS, (max_heap_size, max_heap_size))
 
 if __name__ == '__main__':
     main()
